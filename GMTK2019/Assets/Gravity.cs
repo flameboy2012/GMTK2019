4afc7477c69e0271bc689ec452cb7c8c9a31045e
Daniel <daniel.orford446@gmail.com>
2019-08-02 23:06:14 +0100

    Player Controller and Gravity implemented

---
+++ GMTK2019/Assets/Gravity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    enum GravityDirection { Down, Left, Up, Right };
+    GravityDirection Direction;
+    float GravityStrength = -1.0f;
+    void Start()
+    {
+        Direction = GravityDirection.Down;
+    }
+
+    void FixedUpdate()
+    {
+        switch (Direction)
+        {
+            case GravityDirection.Down:
+                Physics2D.gravity = new Vector2(0, GravityStrength);
+                break;
+            default:
+                Physics2D.gravity = new Vector2(0, GravityStrength);
+                break;
+        }
+    }
+}
